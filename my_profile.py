196f2f7b2c0d0183d7afd18f94ef0dcb4eaea37f
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-06-23 15:46:17 -0500

    removing profiling script

--- my_profile.py
+++
@@ -1,8 +0,0 @@
-from cykdtree.tests import test_kdtree
-import cProfile, pstats
-
-cProfile.run('test_kdtree.time_tree_construction(int(1e6), 10)','restats')
-#cProfile.run('test_kdtree.time_neighbor_search(int(1e6), 10)','restats')
-p = pstats.Stats('restats')
-
-p.sort_stats('tottime').print_stats(10)
